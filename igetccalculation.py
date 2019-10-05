9843b6522f1ef289b783509262bed4b4780d8b5c
SUDHARSAN KRISHNAMURTHY <sudhu@1301-32.student.ohlone.edu>
2019-10-04 18:09:34 -0700

    added specification of functions

--- igetccalculation.py
+++ igetccalculation.py
@@ -5,7 +5,8 @@ path = "book.xlsx"
 inputWorkbook = xlrd.open_workbook(path)
 inputWorksheet = inputWorkbook.sheet_by_index(0)
 
-majors = []
+majors = {}
+
 area1 = []
 area2 = []
 area3a = []
@@ -13,7 +14,9 @@ area3b = []
 area4 = []
 area5a = []
 area5b = []
+
 igetcareas = {"area1": area1, "area2" : area2, "area3a": area3a, "area3b": area3b, "area4": area4, "area5alab": area5a, "area5blab":area5b}
+
 index = 0
 for x in igetcareas:
     for y in range(1,inputWorksheet.ncols):
@@ -22,6 +25,9 @@ for x in igetcareas:
 print(igetcareas)
 
 class inputClassesTaken:
+    """
+    Take in data from UI to create a student profile of classes
+    """
     area1 = []
     area2 = []
     area3 = []
@@ -36,6 +42,20 @@ class inputClassesTaken:
 
 
 
-def compareClasses(inputClassesTaken : inputClassesTaken):
+def compareClasses(inputClassesTaken: inputClassesTaken):
+    """
+    Input classes and compare to lists of classes in area to output list of classes required in each area
+
+    """
     return 1
 
+def compareMajor(major, possibleClasses):
+    """
+    take in string from major and compare to output of compareClasses to shorten possible list
+    """
+    return 1
+
+def calculateSemesters():
+    """
+    Calculate time needed in each semester to finish igetc requirements
+    """
